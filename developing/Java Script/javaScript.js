c71cfbd07492dd7f70ae8abb6eab7464c8ef0919
helloshalini <shalinijaiswal1101@gmail.com>
2025-04-21 22:40:46 +0530

    javascript

---
+++ developing/Java Script/javaScript.js
@@ -0,0 +1,20 @@
+// console.log("Hello world");
+
+// let a = 10;
+// let b = 3;
+// console.log("sum is : ", a+b);
+
+let pen = 10;
+let eraser = 5;
+// let output = "total price :"+ (pen + eraser) + " rupess"
+// console.log("total price :", pen + eraser + " rupess");
+// console.log(output);
+console.log(`total price : ${pen + eraser} rupess`);
+
+// if - else condition
+if(true) {
+    console.log("it is true value");
+}
+else {
+    console.log("it is false value");
+}
