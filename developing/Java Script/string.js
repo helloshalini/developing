03508819140fc71f56adce6a916ee45ee1d0b38c
helloshalini <shalinijaiswal1101@gmail.com>
2025-04-22 18:50:15 +0530

    String

---
+++ developing/Java Script/string.js
@@ -0,0 +1,13 @@
+// let msg = "  hello   ";
+// OR 
+
+// let password = prompt("set the password");
+// console.log(password.trim());
+
+// let password = prompt("set the password");
+// let newPass = password.trim();
+// console.log(newPass);
+
+let msg = "   hello "
+let newMsg = password.trim();
+console.log(newMsg)
