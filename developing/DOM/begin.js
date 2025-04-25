4da48efeb44d13ade4af2c46a1141ed17ed90b33
helloshalini <shalinijaiswal1101@gmail.com>
2025-04-25 23:12:08 +0530

    DOM

---
+++ developing/DOM/begin.js
@@ -0,0 +1,10 @@
+// let smallImages = document.getElementsByClassName("oldImg");
+
+// for(let i = 0; i<smallImages.length; i++) {
+//     console.dir(smallImages[i]);
+//     console.dir(smallImages[i].src);
+//     smallImages[i].src = "spiderman_img.png";
+//     console.log(`value of image number ${i} is changed`)
+
+// }
+
