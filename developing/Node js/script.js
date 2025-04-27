ef85dba0e3f6a124f3057b3fce8b5ee25ee7fcb1
helloshalini <shalinijaiswal1101@gmail.com>
2025-04-27 15:54:15 +0530

    Backend Developer

---
+++ developing/Node js/script.js
@@ -0,0 +1,18 @@
+// let n = 5;
+
+// for(let i = 0 ; i<n ;i++) {
+//     console.log("hello", i);
+// }
+
+// console.log(process.argv);
+
+// const math = require("./math");
+// console.log(math);
+// console.log(math.sum(2,3));
+// console.log(math.PI);
+// console.log(math.g);
+
+
+const info = require("./Node fruit");
+
+console.log(info);
