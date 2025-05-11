3b6d05ee8abc1e3b780ee43b223986cf434d8a77
helloshalini <shalinijaiswal1101@gmail.com>
2025-05-11 10:36:49 +0530

    EJS Template

--- Backend/Node js/EJ_DIR/index.js
+++ Backend/Node js/EJ_DIR/index.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const port = 8080;
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
+});
+
+app.get("/hello", (req, res) => {
+    res.send(" Hello");
+});
+
+// app.get("/rollDice", (req, res) => {
+//     res.render("rolldice.ejs");
+// });
+
+app.get("/rollDice", (req, res) => {
+    let diceVal = Math.floor(Math.random() * 6) + 1;
+    res.render("rolldice.ejs", {num: diceVal});
 });
 
 app.listen(port, () => {
